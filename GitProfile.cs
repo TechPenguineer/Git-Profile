527a3740ed422b249aaf9745e428aa79a7f6f581
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-08-01 23:06:32 -0400

    create api data req

--- GitProfile.cs
+++ GitProfile.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using gitprofile;
 
 namespace gitprofile
 {
     class GitProfile
     {
-        public static void main(string[] args)
+        public static void Main(string[] args)
         {
-
+            string res = API.Api.GetApiData("TechPenguineer");
+            Console.WriteLine(res);
         }
     }
 }
---
+++ lib/Api.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+namespace gitprofile.API
+{
+    class Api
+    {
+        public static string GetApiData(String username)
+        {
+            string url_data = $"https://api.github.com/users/{username}";
+
+            string json = new WebClient().DownloadString("https://api.coinmarketcap.com/v1/ticker/");
+
+            List<Item> items = JsonConvert.DeserializeObject<List<Item>>(json);
+        }
+    }
+}
